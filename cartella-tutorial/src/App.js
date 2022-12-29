d915107b2886b4b9f65ed5ab35e7d8c623a2649f
GabrielePagnotta <gabriele.pagnotta1998@gmail.com>
2022-12-29 13:57:47 +0100

    creazioen di funzioni al click

--- cartella-tutorial/src/App.js
+++ cartella-tutorial/src/App.js
@@ -1,22 +1,21 @@
 import React from "react";
 import ComponenteDiProva from "./ComponenteDiProva"
-import ComponenteDiProva2 from "./ComponenteDiProva2"
 
-const cards = {
-  nome : "Gabriele",
-  cognome : "Pagnotta"
-}
 
-const cards2 = {
-  nome : "Giovanni",
-  cognome : "Magis"
-}
+
+const arraycars=[
+  {auto:"ferrari",cavalli:"300cc"},
+  {auto:"lamborghini",cavalli:"400cc"}
+];
 
 function App() {
   return (
     <div className="App">
-      <ComponenteDiProva nome={cards.nome} cognome={cards.cognome} />
-      <ComponenteDiProva2 {...cards2}/>
+      {arraycars.map((el,index) =>{
+        return <ComponenteDiProva key={index} {...el}/>
+      })}
+      
+      
     </div>
   );
   
--- cartella-tutorial/src/ComponenteDiProva.js
+++ cartella-tutorial/src/ComponenteDiProva.js
@@ -1,10 +1,19 @@
 import React from 'react'
 
- const ComponenteDiProva = ({nome, cognome}) => {
+
+ const ComponenteDiProva = (el) => {
+  
+  const props = el
+  const bottone = ()=>{
+    alert("ciao")
+  }
+ 
   return (
    <div>
-    <h1>{nome}</h1>
-    <h2>{cognome}</h2>
+    <h1>{props.auto}</h1>
+    <h2>{props.cavalli}</h2>
+    <button onClick={()=> alert(`la mia auto si chiama ${props.auto}`)}>clicca qui</button>
+    <button onClick={bottone}>clicca qui</button>
     </div>
   )
 }
